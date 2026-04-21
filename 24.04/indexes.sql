0b807a969e7ac2ddbd3a19392f559eac70265034
Katri Lotamõis <katrilotamois@outlook.com>
2026-04-21 17:00:32 +0300

    add additional index material

--- 24.04/indexes.sql
+++ 24.04/indexes.sql
@@ -32,3 +32,41 @@ with (index(IX_Employee_Salary))
 SELECT * FROM EmployeeWithSalary
 WITH (INDEX(IX_Employee_Salary_Range))
 WHERE Salary > 4000 AND Salary < 7000;
+
+-- INDEKSITE TÜÜBID: --
+-- 1. Klastris olevad - Clustered Index
+-- 2. Mitte klastris olevad - Non-Clustered Index
+-- 3. Unikaalsed - Unique Index
+-- 4. Filtreeritud - Filtered Index
+-- 5. XML
+-- 6. Täistekst - Full-Text Index
+-- 7. Ruumiline - Spatial Index
+-- 8. Veerusäilitav/Veergude indeksid - Columnstore Index
+-- 9. Väljaarvatud veergudega indeksid - Index with Included Columns
+
+-- KOKKUVÕTE --
+-- Kui vajad kiiret otsingut id-järgi, kasuta Clustered (Primary Key).
+--
+-- Kui vajad kiiret otsingut nime/kuupäeva järgi, kasuta Non-Clustered.
+--
+-- Kui analüüsid miljoneid ridu, kasuta Columnstore.
+--
+-- Kui otsid asukohti kaardilt, kasuta Spatial.
+
+-- NÄITED --
+-- Clustered index:
+create table EmployeeCity(
+    Id int primary key,
+    Name nvarchar(25),
+    Salary int,
+    Gender nvarchar(10),
+    City nvarchar(20)
+)
+
+insert into EmployeeWithSalary values(3, 'John', 4500, 'Male')
+insert into EmployeeWithSalary values(1, 'Sam', 2500, 'Male')
+insert into EmployeeWithSalary values(4, 'Sara', 5500, 'Female')
+insert into EmployeeWithSalary values(5, 'Todd', 3100, 'Male')
+insert into EmployeeWithSalary values(2, 'Pam', 6500, 'Female')
+
+-- Andmete sisestamisel tulevad need ikka õiges järjekorras primary key pärast.
